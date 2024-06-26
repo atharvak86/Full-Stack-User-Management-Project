8f8a84a6640d50c270a102b4ea293c476c311d10
atharvak86 <atharvakhadse44@gmail.com>
2024-06-26 16:03:42 +0530

    Initial commit

---
+++ src/main/java/com/model/User.java
@@ -0,0 +1,54 @@
+package com.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "app_user")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(length = 50)
+    private String username;
+    @Column(length = 100)
+    private String name;
+    @Column(length = 200)
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
